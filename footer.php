c3b110e6d64aed08bd2e13eca6bc0bb56b1d4681
Abderrazak Bensallam <bensallam200@gmail.com>
2018-10-28 17:37:10 +0000

    First commit

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!-- Footer Section Start -->
+<footer>
+  <div class="container">
+    <div class="row">
+      <!-- Footer Links -->
+      <div class="col-lg-6 col-sm-6 col-xs-12">
+        <ul class="footer-links">
+          <li>
+            <a href="#">Homepage</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">About Us</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-lg-6 col-sm-6 col-xs-12">
+        <div class="copyright">
+          <p>All copyrights reserved &copy; 2018 - Designed & Developed by <a rel="nofollow" href="https://uideck.com">UIdeck</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- Footer Section End -->
+
+<!-- Go To Top Link -->
+<a href="#" class="back-to-top">
+  <i class="lnr lnr-arrow-up"></i>
+</a>
+
+<div id="loader">
+  <div class="spinner">
+    <div class="double-bounce1"></div>
+    <div class="double-bounce2"></div>
+  </div>
+</div>
+
+<!-- jQuery first, then Tether, then Bootstrap JS. -->
+<script src="<?php bloginfo('template_url'); ?>/js/jquery-min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/popper.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.mixitup.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/nivo-lightbox.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/owl.carousel.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.stellar.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.nav.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/scrolling-nav.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.easing.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/smoothscroll.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.slicknav.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/wow.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.vide.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.counterup.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/waypoints.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/form-validator.min.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/contact-form-script.js"></script>
+<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
+
+</body>
+</html>
